e35c4502e5ccd1ec62a7b5264d65b403bea6273e
Eray <95094747+eraysolenkol@users.noreply.github.com>
2023-07-24 20:15:17 +0300

    Add files via upload

---
+++ selectionSort.js
@@ -0,0 +1,14 @@
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length-1; i++) {
+        let min = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[min] > arr[j]) {
+                min = j;
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
